c89e23c1a90cf788a99378719788f44889891dc6
Nils Ceberg <nils.ceberg@gmail.com>
2016-06-07 17:34:42 +0200

    Make Protocol a class

--- src/network/Protocol.lua
+++ src/network/Protocol.lua
@@ -1,24 +1,26 @@
-local function Protocol(messages)
-	local protocol = { _opcode_map = {} }
+local Protocol = {
+	new = function(messages)
+		local protocol = { _opcode_map = {} }
 
-	for name,message in pairs(messages) do
-		protocol[name] = message
-		protocol._opcode_map[message.opcode] = message
-	end
-
-	-- TODO: decoding opcode here but encoding it in Message.lua is wierd.
-	function protocol.decode(buffer)
-		local opcode = string.unpack(">!1I2", buffer)
-
-		if not protocol._opcode_map[opcode] then
-			error("invalid opcode " .. opcode)
+		for name,message in pairs(messages) do
+			protocol[name] = message
+			protocol._opcode_map[message.opcode] = message
 		end
 
-		return opcode, protocol._opcode_map[opcode].decode(buffer)
-	end
+		-- TODO: decoding opcode here but encoding it in Message.lua is wierd.
+		function protocol.decode(buffer)
+			local opcode = string.unpack(">!1I2", buffer)
 
-	return protocol
-end
+			if not protocol._opcode_map[opcode] then
+				error("invalid opcode " .. opcode)
+			end
+
+			return opcode, protocol._opcode_map[opcode].decode(buffer)
+		end
+
+		return protocol
+	end
+}
 
 return Protocol
 
