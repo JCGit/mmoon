852bf409612995fe820398379c181428da45bfbb
Nils Ceberg <nils.ceberg@gmail.com>
2016-06-13 19:32:56 +0200

    Add util.bind

--- src/util/Util.lua
+++ src/util/Util.lua
@@ -3,5 +3,11 @@ return {
 		return function()
 			return queue:pop()
 		end
+	end,
+	bind = function(func, ...)
+		local bound_args = {...}
+		return function(...)
+			return func(unpack(bound_args), ...)
+		end
 	end
 }
