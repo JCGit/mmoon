1185edb5c1d18ca97a4146bc8cb454cdccad7197
Nils Ceberg <nils.ceberg@gmail.com>
2016-11-20 16:20:22 +0100

    add basic admin console on port 4444

--- src/util/Util.lua
+++ src/util/Util.lua
@@ -1,13 +1,190 @@
-return {
-	pop_all = function(queue)
-		return function()
-			return queue:pop()
+local util = {}
+
+function util.pop_all(queue)
+	return function()
+		return queue:pop()
+	end
+end
+
+function util.bind(func, ...)
+	local bound_args = {...}
+	return function(...)
+		return func(unpack(bound_args), ...)
+	end
+end
+
+function util.switch(a, b)
+	return b, a
+end
+
+function util.range(i, j)
+	return function()
+		if i > j then
+			return nil
 		end
-	end,
-	bind = function(func, ...)
-		local bound_args = {...}
-		return function(...)
-			return func(unpack(bound_args), ...)
+		local val = i
+		i = i + 1
+		return val
+	end
+end
+
+function util.many(n, f, ...)
+	local results = {}
+	for i=1,n do
+		table.insert(results, f(...))
+	end
+	return unpack(results)
+end
+
+function util.map(xs, f, ...)
+	local results = {}
+	for k, x in pairs(xs) do
+		results[k] = f(x, ...)
+	end
+	return results
+end
+
+function util.each(xs, f, ...)
+	for k, x in pairs(xs) do
+		f(x, ...)
+	end
+	return xs
+end
+
+function util.copy(x)
+	if type(x) ~= "table" then
+		return x
+	end
+
+	local c = {}
+	for k, y in pairs(x) do
+		c[k] = util.copy(y)
+	end
+
+	return c
+end
+
+function util.findfirst(xs, x)
+	for i,y in ipairs(xs) do
+		if x == y then return i end
+	end
+	return -1
+end
+
+function util.shallow_copy(x)
+	local c = {}
+	for k, y in pairs(x) do
+		c[k] = y
+	end
+	return c
+end
+
+
+function util.filter(xs, f)
+	if type(f) ~= "function" then
+		local val = f
+		f = function(x)
+			return x == val
 		end
 	end
-}
+	
+	local results = {}
+	for i, x in ipairs(xs) do
+		if not f(x) then
+			table.insert(results, x)
+		end
+	end
+	return results
+end
+
+function util.filter_map(xs, f)
+	if type(f) ~= "function" then
+		local val = f
+		f = function(x)
+			return x == val
+		end
+	end
+	
+	local results = {}
+	for k, x in pairs(xs) do
+		if not f(x) then
+			results[k] = x
+		end
+	end
+	return results
+end
+
+function util.zip_with(f, xs, ys)
+	local results = {}
+	for i, x in ipairs(xs) do
+		table.insert(results, f(x, ys[i]) or util.Empty)
+	end
+	return results
+end
+
+function util.and_(xs)
+	for _, x in ipairs(xs) do
+		if not x then return false end
+	end
+	return true
+end
+
+function util.all(xs, f, ...)
+	return util.and_(util.map(xs, f, ...))
+end
+
+function util.first(x, _)
+	return x
+end
+
+function util.second(_, x)
+	return x
+end
+
+function util.concat(t1, t2)
+	for i, x in ipairs(t2) do
+		table.insert(t1, x)
+	end
+	return t1
+end
+
+-- Returns an array of array t divided into groups of n
+function util.groups(t, n)
+	local r = {}
+	for o = 1,#t,n do
+		table.insert(r, {})
+		for i = o, o+n-1 do
+			table.insert(r[#r], t[i])
+		end
+	end
+	return r
+end
+
+function util.replicate(x, n)
+	local xs = {}
+	for i=1,n do
+		table.insert(xs, x)
+	end
+	return xs
+end
+
+function util.hexdump(data, chunk)
+	chunk = chunk or 12
+	local octets = util.groups({data:byte(1, data:len())}, chunk)
+	for i, group in ipairs(octets) do
+		log.info(("%04x: %s"):format((i-1) * chunk,
+			table.concat(util.map(group, util.bind(string.format, "%02x")),
+				" ")))
+	end
+end
+
+function util.keys(xs)
+	local keys = {}
+	for key, _ in pairs(xs) do
+		table.insert(keys, key)
+	end
+	return keys
+end
+
+return util
+
---
+++ src/world/Console.lua
@@ -0,0 +1,83 @@
+local socket = require "socket"
+local util = require "util.Util"
+
+local Console = require "util.Class"(function(self, env)
+	self._env = env
+
+	self._listener = socket.tcp()
+	self._listener:setoption("reuseaddr", true)
+	self._listener:bind("127.0.0.1", 4444)
+	self._listener:listen(5)
+	self._listener:settimeout(0)
+
+	self._clients = {}
+end)
+
+function Console:update()
+	local socket, err = self._listener:accept()
+	if socket then
+		socket:settimeout(0)
+		self:_welcome(socket)
+		self:_prompt(socket)
+		table.insert(self._clients, { socket = socket, buffer = "" })
+	elseif err ~= "timeout" then
+		error(err)
+	end
+
+
+	table.insert(self._clients, client)
+
+	for i, client in ipairs(self._clients) do
+		local line, err
+		line, err, client.buffer =
+			client.socket:receive("*l", client.buffer)
+
+		if line then
+			self:_command(client, line)
+		elseif err == "closed" then
+			client.closed = true
+		elseif err ~="timeout" then
+			error(err)
+		end
+	end
+
+	self._clients = util.filter(self._clients, function(client)
+		return client.closed
+	end)
+end
+
+function Console:_command(client, command)
+	if command:sub(1,1) == "=" then
+		command = string.format("return (%s)", command:sub(2))
+	end
+
+	local f, err = load(command, "command", "t", self._env)
+	if not f then
+		client.socket:send("error: "..err.."\n")
+	else
+		local success, result = pcall(f, client.socket)
+		if success then
+			if result then
+				client.socket:send(tostring(result))
+				client.socket:send("\n")
+			end
+		else
+			client.socket:send("error: "..result.."\n")
+		end
+	end
+
+	self:_prompt(client.socket)
+end
+
+function Console:_prompt(socket)
+	socket:send("> ")
+end
+
+function Console:_welcome(socket)
+	socket:send("\x1B[37m")
+	socket:send("Welcome to the mmoon admin Lua interpreter console.\n")
+	socket:send("\x1B[0m")
+end
+
+return Console
+
